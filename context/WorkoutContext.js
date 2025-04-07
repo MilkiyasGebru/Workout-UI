a70fede7f4481b5be1ec4e86f07f6d656ba5cef1
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-07 14:57:47 +0300

    Added a workout context provider

--- context/WorkoutContext.js
+++ context/WorkoutContext.js
@@ -1,6 +1,20 @@
-import {createContext} from "react";
+"use client"
+import {createContext, useReducer} from "react";
+import {WorkoutsReducer} from "@/Reducer/WorkoutsReducer";
 
-export const WorkoutContext = createContext();
+export const WorkoutContext = createContext(null);
 
 
 
+export const WorkoutsContextProvider = ({children}) => {
+    const [state, dispatch] = useReducer(WorkoutsReducer, {
+        workouts: [],
+    });
+
+    return (
+        <WorkoutContext.Provider value={{...state, dispatch}}>
+            {children}
+        </WorkoutContext.Provider>
+    )
+
+}
