c471fbeb64eec2275d2794bc3f11af8e224e3d3f
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-07 08:24:19 +0300

    Added the Navbar component

---
+++ components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <nav className="p-4 max-w-[1400px] mx-auto">
+            <div>
+                <h1 className="text-4xl font-bold">Workout Partner</h1>
+            </div>
+        </nav>
+    )
+}
