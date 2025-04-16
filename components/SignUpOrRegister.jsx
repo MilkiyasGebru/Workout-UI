db3284d5fd29f32919c1a2b64751f17f358ebe38
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-16 09:20:27 +0300

    Created a signup or Login Component

---
+++ components/SignUpOrRegister.jsx
@@ -0,0 +1,35 @@
+"use client"
+
+import {useState} from "react";
+
+export default function SignUpOrRegister ({name}){
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmitForm = (e) => {
+        e.preventDefault();
+        console.log(email, password);
+    }
+
+    return (
+        <form action="" className="p-4 border border-gray-100 rounded-md flex flex-col w-3/5 xl:w-1/3 bg-white" onSubmit={handleSubmitForm}>
+            
+            <h3 className="text-2xl text-center font-semibold "> {name}</h3>
+
+            <label htmlFor="email" className="text-xl mt-4">Email:</label>
+            <input type="text" id="email" value={email}
+                   className="border border-gray-200 px-3 py-2 outline-none rounded-md w-2/3"
+                   onChange={(e) => setEmail(e.target.value)}/>
+
+
+            <label htmlFor="password" className="mt-4 text-xl">Password:</label>
+            <input type="password" id="password"
+                   className="border border-gray-200 outline-none px-3 py-2 rounded-md w-2/3"
+                   value={password} onChange={(e) => setPassword(e.target.value)}/>
+
+            <button className="px-3 py-2 rounded-md border border-gray-100 w-fit mt-4 bg-lime-200 text-gray-600 cursor-pointer" onClick={handleSubmitForm}>{name}</button>
+
+        </form>
+    )
+
+}
