7d4d9e9219d0801a57d661f79b9995583641c6af
Miki@0929054164 <milkiyasgebru@gmail.com>
2025-04-16 09:21:06 +0300

    Created a login page

---
+++ app/login/layout.js
@@ -0,0 +1,8 @@
+export default function LoginLayout({children}) {
+
+    return (
+        <div className="flex justify-center items-center bg-gray-100 min-h-screen">
+            {children}
+        </div>
+    )
+}
